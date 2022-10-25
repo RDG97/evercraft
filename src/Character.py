4e9247230b34ffaa8414ece1bad7bfe17e0732dc
BellowingButcher <mason.williams@student.awesomeinc.org>
2022-10-25 19:26:05 +0000

    finished ability score array

--- src/Character.py
+++ src/Character.py
@@ -1,5 +1,13 @@
 class Character:
-    def __init__(self, Name, alignment):
+    strength = 10
+    # strength_mod = strength + mod
+    dexterity = 10
+    constitution = 10
+    wisdom = 10
+    intelligence = 10
+    charisma = 10
+    ability_scores = [strength, dexterity, constitution, wisdom, intelligence, charisma]
+    def __init__(self, Name, alignment, ability_scores):
         self.name = Name
         self.alignment = alignment
         self.hit_points = 5
@@ -7,3 +15,4 @@ class Character:
         self.is_alive = True
         self.xp = 0
         self.armour_class = 10
+        self.ability_scores = ability_scores
--- src/tests/test_character.py
+++ src/tests/test_character.py
@@ -5,41 +5,104 @@ def test_character():
 def test_character_attribute():
     name = 'Ryan'
     alignment = 'good man'
-    character = Character(name, alignment)
+    strength = 10
+    dexterity = 10
+    constitution = 10
+    wisdom = 10
+    intelligence = 10
+    charisma = 10
+    ability_scores = [strength, dexterity, constitution, wisdom, intelligence, charisma]
+
+    character = Character(name, alignment, ability_scores)
     assert character.name != None
 
 def test_character_alignment():
     name = 'Ryan'
     alignment = 'good'
-    character = Character(name, alignment)
+    strength = 10
+    dexterity = 10
+    constitution = 10
+    wisdom = 10
+    intelligence = 10
+    charisma = 10
+    ability_scores = [strength, dexterity, constitution, wisdom, intelligence, charisma]
+    character = Character(name, alignment, ability_scores)
     assert character.alignment != None
 
 def test_character_hit_points():
     name = 'Ryan'
     alignment = 'good'
-    character = Character(name, alignment)
+    strength = 10
+    dexterity = 10
+    constitution = 10
+    wisdom = 10
+    intelligence = 10
+    charisma = 10
+    ability_scores = [strength, dexterity, constitution, wisdom, intelligence, charisma]
+    character = Character(name, alignment, ability_scores)
     assert character.hit_points > 0
 
 def test_character_attack_points():
     name = 'Ryan'
     alignment = 'good'
-    character = Character(name, alignment)
+    strength = 10
+    dexterity = 10
+    constitution = 10
+    wisdom = 10
+    intelligence = 10
+    charisma = 10
+    ability_scores = [strength, dexterity, constitution, wisdom, intelligence, charisma]
+    character = Character(name, alignment, ability_scores)
     assert character.attack_points > 0
 
 def test_is_alive():
     name = 'Ryan'
     alignment = 'good'
-    character = Character(name, alignment)
+    strength = 10
+    dexterity = 10
+    constitution = 10
+    wisdom = 10
+    intelligence = 10
+    charisma = 10
+    ability_scores = [strength, dexterity, constitution, wisdom, intelligence, charisma]
+    character = Character(name, alignment, ability_scores)
     assert character.is_alive == True
 
 def test_xp():
     name = 'Ryan'
     alignment = 'good'
-    character = Character(name, alignment)
+    strength = 10
+    dexterity = 10
+    constitution = 10
+    wisdom = 10
+    intelligence = 10
+    charisma = 10
+    ability_scores = [strength, dexterity, constitution, wisdom, intelligence, charisma]
+    character = Character(name, alignment, ability_scores)
     assert character.xp >= 0
 
 def test_armour_class():
     name = 'Ryan'
     alignment = 'good'
-    character = Character(name, alignment)
+    strength = 10
+    dexterity = 10
+    constitution = 10
+    wisdom = 10
+    intelligence = 10
+    charisma = 10
+    ability_scores = [strength, dexterity, constitution, wisdom, intelligence, charisma]
+    character = Character(name, alignment, ability_scores)
     assert character.armour_class > 0
+
+def test_armour_class():
+    name = 'Ryan'
+    alignment = 'good'
+    strength = 10
+    dexterity = 10
+    constitution = 10
+    wisdom = 10
+    intelligence = 10
+    charisma = 10
+    ability_scores = [strength, dexterity, constitution, wisdom, intelligence, charisma]
+    character = Character(name, alignment, ability_scores)
+    assert len(character.ability_scores) == 6 
